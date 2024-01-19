6b8b88670bd89fd86fef9b7c422dd39312c66929
Laconich <jlaconich97@gmail.com>
2024-01-19 19:34:36 -0300

    parseo

---
+++ el_parseo.py
@@ -0,0 +1,15 @@
+nombre = "Abigail"
+apellido = "Lopez"
+edad = 88
+peso = 75
+
+
+print(type(nombre))
+print(type(edad))
+print(type(peso))
+
+#Parseo
+
+edadStr = str(nombre)
+
+print(f"la edad anterior es {type(edad)}, La edad parseada es de tipo {type(edadStr)}")
