6c273c4ce33a0a3400d5bd9c7b7e5ff518cb74ed
Laconich <jlaconich97@gmail.com>
2024-01-19 19:22:17 -0300

    Pitagoras

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+import math
+
 print("Hola desde Loma")
-//
+
+
+#Aplicar Teorema de Pitagoras
+catetoA = 2
+catetoB = 5
+
+Hip = math.sqrt(pow(catetoA,2) + pow(catetoB,2))
+
+print(f"La hipotenusa es {Hip}")
